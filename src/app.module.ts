3cc88f06aefe742490b4a02d877b3a356f863497
Jhony Souza <jhony.jss@gmail.com>
2023-06-26 15:58:03 -0300

    chore: database connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CategoriesModule, NotificationsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mariadb',
+      port: 3306,
+      username: 'gila',
+      password: 'gila',
+      database: 'gilaDB',
+      entities: [],
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+    CategoriesModule,
+    NotificationsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
